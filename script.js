589cade0605f74bc352444ad6057510958c36cae
waelha <waelalkiyani@gmail.com>
2024-06-24 09:31:19 +0300

    The version includes js edit and a new text file

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ document.querySelector('.burger-menu').addEventListener('click', function() {
     document.querySelector('.nav-links').classList.toggle('active');
   });
   
+
+  console.log("Testing ...")
+  onsole.log("Testing 2 ...")
